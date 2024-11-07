676d301e669d447526ad22fe8f96f1598137d0bb
George Thomas <georgefsthomas@gmail.com>
2024-11-07 18:15:44 +0000

    Style nodes
    We mostly copy the "expert" layout from the React frontend at https://github.com/hackworthltd/primer-app very closely.
    
    This code is _much_ more concise than the equivalent TypeScript/React/Tailwind code, even if the lack of support for non-expert levels makes a precise comparison difficult. This is down to the power of Haskell, the hindsight afforded by a rewrite, and Tailwind's lack of compositionality along certain axes (it was impossible to really factor out using the same color as a part of multiple attributes, since Tailwind doesn't allow concatenating strings to form class names). In reverse-engineering that mess of code and teasing out its structure, we uncover several oddities which we don't replicate here:
    - We set some text colors which were completely overridden, such as `text-grey-tertiary` for most nodes.
    - We used `text-red-primary` instead of `text-red-tertiary` for type-level holes.
    - We set the background twice instead of ever setting the border for `KType` and `KFun`, in `flavorLabelClasses`.
    
    Other differences:
    - We show the full word "match" on pattern match nodes, instead of just "m". This was always wrong but awkward to fix with the old architecture.
    - We scale kind nodes down slightly rather than telling the layout engine to treat them as a different size to other syntax nodes. This is slightly easier to implement, and arguably looks better.
    - We use colours for kinds consistent with type/term analogues, e.g. `KHole` is now red. Again, this is somewhat implementation driven (the new code, being much less ad-hoc, generally makes consistency the easier option), but is also probably an improvement.
    
    As well as adding CSS styles, we also now layout based on actual node widths and heights. This could have probably been made a separate commit, but there were advantages to developing both features alongside each other. The styled nodes would have been difficult to view without the improved layout, and the requirements for getting the information needed for the layout engine had some impact on how we approached styling.
    
    We avoid changing the tree structure, since we're already doing a lot in one commit. Therefore better rendering of bindings and patterns will wait for a follow-up.

--- app/Primer.hs
+++ app/Primer.hs
@@ -10,6 +10,7 @@ module Primer (start) where
 
 import Foreword
 
+import Colours
 import Control.Monad.Extra (eitherM)
 import Control.Monad.Fresh (MonadFresh (..))
 import Data.Aeson (FromJSON, ToJSON)
@@ -29,7 +30,7 @@ import Primer.Core qualified as Primer
 import Primer.Core.Utils (forgetTypeMetadata)
 import Primer.Def (ASTDef (..), Def (..), astDefExpr)
 import Primer.Module (Module (moduleDefs, moduleName))
-import Primer.Name (NameCounter, unName)
+import Primer.Name (Name, NameCounter, unName)
 import Primer.Typecheck (ExprT, TypeError, check, checkKind)
 
 start :: JSM ()
@@ -97,113 +98,231 @@ viewModel Model{..} =
             ]
         ]
 
+-- | A renderable node with dimensions.
+data NodeView action = NodeView
+    { view :: View action
+    , width :: Int
+    , height :: Int
+    }
+    deriving (Generic)
+
+data NodeViewData
+    = SyntaxNode {wide :: Bool, color :: Text, text :: Text}
+    | HoleNode {empty :: Bool}
+    | PrimNode PrimCon
+    | ConNode {name :: Name, scope :: ModuleName}
+    | VarNode {name :: Name, mscope :: Maybe ModuleName} -- TODO we should be able to re-use the name `scope`: https://github.com/ghc-proposals/ghc-proposals/pull/535#issuecomment-1694388075
+
+mkNodeView :: NodeViewData -> Map Text Text -> Map Text Text -> NodeView action
+mkNodeView opts extraOuterStyles extraInnerStyles =
+    NodeView
+        { width
+        , height
+        , view = case opts of
+            PrimNode (PrimAnimation animation) ->
+                img_
+                    [ src_ ("data:img/gif;base64," <> animation)
+                    , style_ $
+                        [ ("width", show width <> "px")
+                        , ("height", show height <> "px")
+                        ]
+                            <> extraOuterStyles
+                            <> extraInnerStyles
+                    ]
+            _ ->
+                div_
+                    [ style_ $
+                        [ ("display", "flex")
+                        , ("justify-content", "center")
+                        , ("align-items", "center")
+                        , ("border-style", "solid")
+                        , ("box-sizing", "border-box")
+                        , ("border-color", borderColor)
+                        , ("background-color", backgroundColor)
+                        , ("color", fontColor)
+                        , ("width", show width <> "px")
+                        , ("height", show height <> "px")
+                        , ("border-width", ".25rem")
+                        , ("padding-left", ".25rem")
+                        , ("padding-right", ".25rem")
+                        ]
+                            <> case opts of
+                                HoleNode{} -> [("font-style", "italic")]
+                                _ -> []
+                            <> extraOuterStyles
+                    ]
+                    [ div_
+                        [ style_ $
+                            [ ("overflow", "hidden")
+                            , ("text-overflow", "ellipsis")
+                            , ("white-space", "nowrap")
+                            ]
+                                <> extraInnerStyles
+                        ]
+                        [ text case opts of
+                            SyntaxNode{text} -> text
+                            HoleNode{empty} -> if empty then "?" else "⚠️"
+                            PrimNode pc -> case pc of
+                                PrimChar c' -> show c'
+                                PrimInt n -> show n
+                            ConNode{name} -> unName name
+                            VarNode{name} -> unName name
+                        ]
+                    ]
+              where
+                borderColor = case opts of
+                    SyntaxNode{..} -> color
+                    HoleNode{} -> redTertiary
+                    PrimNode{} -> greenPrimary
+                    ConNode{} -> greenPrimary
+                    VarNode{} -> blueQuaternary
+                (backgroundColor, fontColor) = case opts of
+                    SyntaxNode{..} -> (color, whitePrimary)
+                    _ -> (whitePrimary, bluePrimary)
+        }
+  where
+    width = case opts of
+        SyntaxNode{wide = False} -> height
+        _ -> 80
+    height = 35
+
 viewTreeExpr ::
     (Data a, Data b, Data c) =>
     Expr' a b c ->
-    Tree.Tree (View (TermMeta' a b c))
-viewTreeExpr e = Tree.Node viewNode viewChildren
+    Tree.Tree (NodeView (TermMeta' a b c))
+viewTreeExpr e =
+    Tree.Node
+        ( over #view (div_ [onClick $ Left $ e ^. _exprMetaLens] . pure) $
+            mkNodeView
+                viewNode
+                -- Curved nodes to indicate value-level expressions.
+                [("border-radius", "1.5rem")]
+                []
+        )
+        viewChildren
   where
-    viewNode = div_
-        [onClick $ Left $ e ^. _exprMetaLens]
-        $ pure case e of
-            Hole{} -> textNode "⚠️"
-            EmptyHole{} -> textNode "?"
-            Ann{} -> textNode ":"
-            Primer.App{} -> textNode "←"
-            APP{} -> textNode "←"
-            Con _ c _ -> textNode $ gname c
-            Lam _ v _ -> textNode $ "λ" <> lname v
-            LAM _ v _ -> textNode $ "Λ" <> lname v
-            Var _ (GlobalVarRef v) -> textNode $ gname v
-            Var _ (LocalVarRef v) -> textNode $ lname v
-            Let{} -> textNode "let"
-            LetType{} -> textNode "let type"
-            Letrec{} -> textNode "let rec"
-            PrimCon _ c -> case c of
-                PrimChar c' -> textNode $ show c'
-                PrimInt n -> textNode $ show n
-                PrimAnimation a -> img_ [src_ ("data:img/gif;base64," <> a)]
-            Case{} -> textNode "match"
+    viewNode = case e of
+        Hole{} -> HoleNode{empty = False}
+        EmptyHole{} -> HoleNode{empty = False}
+        Ann{} -> SyntaxNode False blackPrimary ":"
+        Primer.App{} -> SyntaxNode False blueTertiary "←"
+        APP{} -> SyntaxNode False blueTertiary "←"
+        Con _ c _ -> ConNode{name = baseName c, scope = qualifiedModule c}
+        Lam _ v _ -> SyntaxNode False bluePrimary $ "λ" <> unName (unLocalName v)
+        LAM _ v _ -> SyntaxNode False blueSecondary $ "Λ" <> unName (unLocalName v)
+        Var _ (GlobalVarRef v) -> VarNode{name = baseName v, mscope = Just $ qualifiedModule v}
+        Var _ (LocalVarRef v) -> VarNode{name = unLocalName v, mscope = Nothing}
+        Let{} -> SyntaxNode False blueQuaternary "let"
+        LetType{} -> SyntaxNode False blueQuaternary "let type"
+        Letrec{} -> SyntaxNode False blueQuaternary "let rec"
+        PrimCon _ c -> PrimNode c
+        Case{} -> SyntaxNode True yellowPrimary "match"
     viewChildren = case e of
         Case _ scrut branches fb ->
             viewTreeExpr scrut
                 : ( branches
                         <&> \(CaseBranch p bindings r) ->
                             Tree.Node
-                                ( div_
-                                    []
-                                    $ ( text case p of
-                                            PatCon c -> gname c
-                                            PatPrim c -> case c of
-                                                PrimChar c' -> show c'
-                                                PrimInt n -> show n
-                                                -- This branch should never actually be triggered,
-                                                -- since such programs can't be constructed.
-                                                PrimAnimation _ -> "error: can't pattern match on animation"
-                                      )
-                                        : concatMap (\(Bind _ v) -> [text " ", text $ lname v]) bindings
+                                ( NodeView
+                                    { width = 25 -- TODO just an approximation until we render patterns properly
+                                    , height = 25 -- TODO ditto
+                                    , view =
+                                        div_
+                                            []
+                                            $ ( text case p of
+                                                    PatCon c -> unName $ baseName c
+                                                    PatPrim c -> case c of
+                                                        PrimChar c' -> show c'
+                                                        PrimInt n -> show n
+                                                        -- This branch should never actually be triggered,
+                                                        -- since such programs can't be constructed.
+                                                        PrimAnimation _ -> "error: can't pattern match on animation"
+                                              )
+                                                : concatMap (\(Bind _ v) -> [text " ", text $ unName $ unLocalName v]) bindings
+                                    }
                                 )
                                 [viewTreeExpr r]
                   )
                     <> case fb of
                         CaseExhaustive -> []
-                        CaseFallback r -> [Tree.Node (textNode "_") [viewTreeExpr r]]
+                        CaseFallback r -> [Tree.Node (mkNodeView (SyntaxNode False yellowPrimary "_") [] []) [viewTreeExpr r]]
         _ -> map viewTreeType (e ^.. typesInExpr) <> map viewTreeExpr (children e)
-    textNode t = div_ [] [text t]
 
 viewTreeType ::
     (Data b, Data c) =>
     Type' b c ->
-    Tree.Tree (View (TermMeta' a b c))
-viewTreeType t = Tree.Node viewNode viewChildren
+    (Tree.Tree (NodeView (TermMeta' a b c)))
+viewTreeType t =
+    Tree.Node
+        ( over #view (div_ [onClick $ Right $ Left $ t ^. _typeMetaLens] . pure) $
+            mkNodeView viewNode [] []
+        )
+        viewChildren
   where
-    viewNode = div_
-        [onClick $ Right $ Left $ t ^. _typeMetaLens]
-        $ pure case t of
-            TEmptyHole{} -> text "?"
-            THole{} -> text "⚠️"
-            TCon _ c -> text $ gname c
-            TFun{} -> text "→"
-            TVar _ v -> text $ lname v
-            TApp{} -> text "←"
-            TForall _ v _ _ -> text $ "∀" <> lname v
-            TLet{} -> text "let"
+    viewNode = case t of
+        TEmptyHole{} -> HoleNode{empty = True}
+        THole{} -> HoleNode{empty = True}
+        TCon _ c -> ConNode{name = baseName c, scope = qualifiedModule c}
+        TFun{} -> SyntaxNode False bluePrimary "→"
+        TVar _ v -> VarNode{name = unLocalName v, mscope = Nothing}
+        TApp{} -> SyntaxNode False blueTertiary "←"
+        TForall _ v _ _ -> SyntaxNode False blueSecondary $ "∀" <> unName (unLocalName v)
+        TLet{} -> SyntaxNode False blueQuaternary "let"
     viewChildren = map viewTreeKind (t ^.. kindsInType) <> map viewTreeType (children t)
 
-viewTreeKind :: (Data c) => Kind' c -> Tree.Tree (View (TermMeta' a b c))
-viewTreeKind k = Tree.Node viewNode viewChildren
+viewTreeKind :: (Data c) => Kind' c -> Tree.Tree (NodeView (TermMeta' a b c))
+viewTreeKind k =
+    Tree.Node
+        ( over #view (div_ [onClick $ Right $ Right $ k ^. _kindMetaLens] . pure) $
+            mkNodeView
+                viewNode
+                -- Rotate to indicate kind.
+                -- We then scale by (1 + 1/√2)/2 so that dimensions used for layout are a good approximation.
+                [("transform", "rotate(45deg) scale(0.854)")]
+                -- Rotate the content back to it's correct orientation.
+                [("transform", "rotate(-45deg)")]
+        )
+        viewChildren
   where
-    viewNode = div_
-        [onClick $ Right $ Right $ k ^. _kindMetaLens]
-        $ pure case k of
-            KType{} -> text "*"
-            KFun{} -> text "→"
-            KHole{} -> text "?"
+    viewNode = case k of
+        KHole{} -> HoleNode{empty = True}
+        KType{} -> SyntaxNode False greenPrimary "*"
+        KFun{} -> SyntaxNode False bluePrimary "→"
     viewChildren = map viewTreeKind (children k)
 
-gname :: GlobalName k -> Text
-gname = unName . baseName
-lname :: LocalName k -> Text
-lname = unName . unLocalName
-
-viewTree :: Tree (View action) -> View action
-viewTree =
+viewTree :: Tree (NodeView action) -> View action
+viewTree t@(Tree.Node NodeView{width = rootWidth, height = rootHeight} _) =
     -- TODO consider taking top-level attributes and `Tree ([Attribute action] -> View action)`
     -- in order to avoid so many nested `div`s
-    div_ []
+    div_ [style_ [("padding", show (padding / 2) <> "px")]]
         . map
-            ( \(v, P2 x y) ->
+            ( \(node, P2 x y) ->
                 div_
                     [ style_
                         [ ("position", "absolute")
-                        , ("transform", "translate(" <> show x <> "px, " <> show -y <> "px)")
+                        ,
+                            ( "transform"
+                            , "translate("
+                                <> show (x - fromIntegral node.width / 2 - fromIntegral rootWidth / 2)
+                                <> "px,"
+                                <> show (-y - fromIntegral node.height + fromIntegral rootHeight)
+                                <> "px)"
+                            )
                         ]
                     ]
-                    [v]
+                    [node.view]
             )
         . toList
-        -- TODO these values are quite arbitrary and will require tweaking
-        . symmLayout' @Double (def & slHSep .~ 30 & slVSep .~ 40)
+        $ symmLayout' @Double
+            ( def
+                & (slHSep .~ padding)
+                & (slVSep .~ padding)
+                & (slWidth .~ \node -> (-(fromIntegral node.width / 2), fromIntegral node.width / 2))
+                & (slHeight .~ \node -> (0, fromIntegral node.height))
+            )
+            t
+  where
+    padding = 20
 
 -- TODO upstream: https://github.com/dmjio/miso/issues/749
 startAppWithSavedState :: forall model action. (Eq model, FromJSON model, ToJSON model) => Miso.App model action -> JSM ()
